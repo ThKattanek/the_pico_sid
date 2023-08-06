91bd87697bb9e275083c1fb09bce1b8ffd4565ab
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2023-08-06 20:28:34 +0200

    added first pio assembler program

--- firmware/main.c
+++ firmware/main.c
@@ -12,11 +12,13 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
 #include <hardware/gpio.h>
+#include <hardware/pio.h>
+#include <hardware/clocks.h>
 #include <pico/binary_info.h>
 
-#include "./sid.h"
+#include "read_sid_reg.pio.h"
 
-const uint LED_PIN = 25;
+#include "./sid.h"
 
 const uint RES_PIN = 2;
 const uint CLK_PIN = 3;
@@ -25,54 +27,9 @@ const uint CS_PIN = 5;
 
 volatile static bool reset_state = true;
 
-void ResetPinCallback(uint gpio, uint32_t events)
-{
-    switch(gpio)
-    {
-    case RES_PIN:
-        if(events & GPIO_IRQ_EDGE_RISE)
-            reset_state = true;
-        else if(events & GPIO_IRQ_EDGE_FALL)
-            reset_state = false;
-        break;
-
-    case CS_PIN:
-        if(events & GPIO_IRQ_EDGE_RISE)
-        {
-#ifndef NDEBUG
-            printf("CS Signal is edge rise.\n");
-#endif
-        }
-
-        if(events & GPIO_IRQ_EDGE_FALL)
-        {
-#ifndef NDEBUG
-            printf("CS Signal is edge fall.\n");
-#endif
-        }
-        break;
-
-    case RW_PIN:
-        if(events & GPIO_IRQ_EDGE_RISE)
-        {
-#ifndef NDEBUG
-            printf("RW Signal is edge rise.\n");
-#endif
-        }
-
-        if(events & GPIO_IRQ_EDGE_FALL)
-        {
-#ifndef NDEBUG
-            printf("RW Signal is edge fall.\n");
-#endif
-        }
-        break;
-    }
-}
-
 int main() {
     bi_decl(bi_program_description("The Pico SID"));
-    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+    bi_decl(bi_1pin_with_name(PICO_DEFAULT_LED_PIN, "On-board LED"));
     bi_decl(bi_1pin_with_name(RES_PIN, "C64 Reset"));
     bi_decl(bi_1pin_with_name(CLK_PIN, "C64 Clock"));
     bi_decl(bi_1pin_with_name(RW_PIN, "C64 SID RW"));
@@ -80,6 +37,20 @@ int main() {
 
     stdio_init_all();
 
+	// PIO Program initialize
+	PIO pio = pio0;
+
+	uint offset = pio_add_program(pio, &read_sid_reg_program);
+	uint sm = pio_claim_unused_sm(pio, true);
+
+	read_sid_reg_program_init(pio, sm, offset, PICO_DEFAULT_LED_PIN);
+	pio_sm_set_enabled(pio, sm, true);
+
+	uint32_t LED_FREQ = 10;
+	pio->txf[sm] = (clock_get_hz(clk_sys) / (2 * LED_FREQ)) - 3;
+	
+
+/*
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     gpio_put(LED_PIN, 1);
@@ -94,20 +65,17 @@ int main() {
     gpio_set_dir(CS_PIN, GPIO_IN);
 
     // Init GPIO IRQ for RES_PIN
-    gpio_set_irq_enabled_with_callback(RES_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &ResetPinCallback);
-    gpio_set_irq_enabled(CS_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
-    gpio_set_irq_enabled(RW_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    //gpio_set_irq_enabled_with_callback(RES_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &ResetPinCallback);
+    //gpio_set_irq_enabled(CS_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    //gpio_set_irq_enabled(RW_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
 
+	gpio_set_irq_enabled_with_callback(CS_PIN,GPIO_IRQ_EDGE_RISE, true, &ResetPinCallback);
+*/
+	printf("The Pico SID by Thorsten Kattanek\n");
+ 
     SidReset();
 
     while (1)
     {
-        if(!reset_state)
-        {
-            SidReset();
-#ifndef NDEBUG
-            printf("SID RESET. \n");
-#endif
-        }
     }
 }
