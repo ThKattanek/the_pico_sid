15fadf10d69651908d6be8fbb5b0264fb594d750
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2023-08-08 09:33:17 +0200

    change read_sid_reg.pio to write_sid_reg.pio

--- firmware/main.c
+++ firmware/main.c
@@ -16,7 +16,7 @@
 #include <hardware/clocks.h>
 #include <pico/binary_info.h>
 
-#include "read_sid_reg.pio.h"
+#include "write_sid_reg.pio.h"
 
 #include "./sid.h"
 
@@ -40,42 +40,24 @@ int main() {
 	// PIO Program initialize
 	PIO pio = pio0;
 
-	uint offset = pio_add_program(pio, &read_sid_reg_program);
+	uint offset = pio_add_program(pio, &write_sid_reg_program);
 	uint sm = pio_claim_unused_sm(pio, true);
 
-	read_sid_reg_program_init(pio, sm, offset, PICO_DEFAULT_LED_PIN);
+	write_sid_reg_program_init(pio, sm, offset, CS_PIN);
 	pio_sm_set_enabled(pio, sm, true);
 
-	uint32_t LED_FREQ = 10;
-	pio->txf[sm] = (clock_get_hz(clk_sys) / (2 * LED_FREQ)) - 3;
+	pio->txf[sm] = 0;	
 	
-
-/*
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 1);
-
-    // Init Ext Reset
-    gpio_set_dir(RES_PIN, GPIO_IN);
-    // Init Ext Clock
-    gpio_set_dir(CLK_PIN, GPIO_IN);
-    // Init Ext RW
-    gpio_set_dir(RW_PIN, GPIO_IN);
-    // Init Ext CS
-    gpio_set_dir(CS_PIN, GPIO_IN);
-
-    // Init GPIO IRQ for RES_PIN
-    //gpio_set_irq_enabled_with_callback(RES_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &ResetPinCallback);
-    //gpio_set_irq_enabled(CS_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
-    //gpio_set_irq_enabled(RW_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
-
-	gpio_set_irq_enabled_with_callback(CS_PIN,GPIO_IRQ_EDGE_RISE, true, &ResetPinCallback);
-*/
 	printf("The Pico SID by Thorsten Kattanek\n");
  
     SidReset();
 
+	uint t = 0;
+
     while (1)
     {
+		while (!(pio->irq & (1<<0))); // wait for irq 0 fro either of the pio programs
+    		pio_interrupt_clear(pio, 0); // clear the irq so we can wait for it again
+		printf("IRQ_0%d\n",t++);
     }
 }
