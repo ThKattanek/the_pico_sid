66a36cf80976216de1c72e4d48b612981e0f471a
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2023-09-13 19:27:10 +0200

    firmware: new project structur

--- firmware/main.c
+++ firmware/main.c
@@ -19,41 +19,50 @@
 
 #include "sid.h"
 
+// PIN MAPPING
 #define RES_PIN 2
 #define CS_PIN 3
 #define CLK_PIN 4	
 #define CLK_2_PIN 20	// 1/2 CLK from CLK_PIN
 #define RW_PIN 5
-
 #define AUDIO_PIN 28
-
 #define DEBUG_LED_PIN 19
 
-volatile static bool reset_state = true;
+volatile bool reset_state = true;
 
-PIO pio;
-uint sm;
+volatile PIO pio;
+volatile uint sm;
 
-uint slice_num;
+volatile uint slice_num;
 
-static SID sid1;
+//#define RINGBUFFER_ENABLE
+
+#ifdef RINGBUFFER_ENABLE
+volatile uint16_t register_ring_buffer[0x1000];
+volatile uint16_t register_ring_buffer_rpos = 0;
+volatile uint16_t register_ring_buffer_wpos = 0;
+#endif
+
+bool led_state = true;
 
 void InitPWMAudio(uint audio_out_gpio);
 
 void WriteSidReg()
 {
-	uint16_t incomming;
-
 	if (pio0_hw->irq & 1) 
 	{
-    	pio0_hw->irq = 1;
+		pio0_hw->irq = 1;
 
-		incomming = pio->rxf[sm];
-		SidWriteReg(&sid1, incomming >> 2, (incomming >> 7) & 0xff);
+#ifdef RINGBUFFER_ENABLE
+		uint16_t incomming = pio->rxf[sm];
+		register_ring_buffer[register_ring_buffer_wpos & 0xfff] = pio->rxf[sm];	
+		register_ring_buffer_wpos++;
+#else
+		uint16_t incomming = pio->rxf[sm];
+		SidWriteReg(incomming >> 2, (incomming >> 7) & 0xff);
+#endif
 	}
 }
-
-bool led_state = true;
 
 void Core1Entry() 
 {
@@ -61,44 +70,12 @@ void Core1Entry()
 	{
 		while(!gpio_get(CLK_2_PIN)){}
 
-		// One Cycle SID
+		// Debug LED
 		led_state = !led_state;
 		gpio_put(DEBUG_LED_PIN, led_state);
 
-		OscExecuteCycles(&sid1.Osc[0], 4);
-		EnvExecuteCycles(&sid1.Env[0], 4);
-		
-		//int voice1 = ((OscGetOutput(&sid1.Osc[0]) * 0xfff - sid1.Filter.WaveZero) * EnvGetOutput(&sid1.Env[0]) + sid1.Filter.VoiceDC);
-		//int voice2 = ((OscGetOutput(&sid1.Osc[1]) * 0xfff - sid1.Filter.WaveZero) * EnvGetOutput(&sid1.Env[1]) + sid1.Filter.VoiceDC);
-		//int voice3 = ((OscGetOutput(&sid1.Osc[2]) * 0xfff - sid1.Filter.WaveZero) * EnvGetOutput(&sid1.Env[2]) + sid1.Filter.VoiceDC);
-
-
-
-		//int voice1 = ((OscGetOutput(&sid1.Osc[0]) / (float)0xfff) * (EnvGetOutput(&sid1.Env[0]) / (float)0xff)) * 0xfff;
-		//int voice2 = (OscGetOutput(&sid1.Osc[1]) / (float)0xfff) * (EnvGetOutput(&sid1.Env[1]) / (float)0xff) * 0x3ff;
-
-		
-		
-		//int voice1 = ((int)OscGetOutput(&sid1.Osc[0] - sid1.Filter.WaveZero) * (int)EnvGetOutput(&sid1.Env[0]) + sid1.Filter.VoiceDC);
-		//int voice2 = ((int)OscGetOutput(&sid1.Osc[1] - sid1.Filter.WaveZero) * (int)EnvGetOutput(&sid1.Env[1]) + sid1.Filter.VoiceDC);
-		//int voice3 = ((int)OscGetOutput(&sid1.Osc[2] - sid1.Filter.WaveZero) * (int)EnvGetOutput(&sid1.Env[2]) + sid1.Filter.VoiceDC);
-
-		int voice1 = OscGetOutput(&sid1.Osc[0]) >> 2;
-		int voice2 = OscGetOutput(&sid1.Osc[1]) >> 2;
-		int voice3 = OscGetOutput(&sid1.Osc[2]) >> 2;
-
-/*
-		voice1 >>= 7;
-		voice2 >>= 7;
-		voice3 >>= 7;
-	
-
-		int voice1 = OscGetOutput(&sid1.Osc[0]);
-		int voice2 = OscGetOutput(&sid1.Osc[1]);
-		int voice3 = OscGetOutput(&sid1.Osc[2]);
-			*/
-
-		FilterExcecuteCycles(&sid1.Filter, 4, voice1, voice2, voice3, 0);
+		// every 4 cycles
+		SidCycle(4);
 
 		while(gpio_get(CLK_2_PIN)){}
 	}
@@ -117,14 +94,22 @@ int main() {
 	gpio_set_dir(DEBUG_LED_PIN, true);
 	gpio_put(DEBUG_LED_PIN, led_state);
 
-	// CLK 1/2
+	// CLK 1/4
 	gpio_set_function(CLK_2_PIN, GPIO_FUNC_SIO);
 	gpio_set_dir(CLK_2_PIN, false);
 
 	// Init SID
-	SidInit(&sid1);
-	SidSetModel(&sid1, MOS_8580);
+	SidInit();
 
+	// Init Audio PWM
+	InitPWMAudio(AUDIO_PIN);
+	
+	// UART Start Message PicoSID
+	printf("The Pico SID by Thorsten Kattanek\n");
+
+	// Start Core#1 for SID Emualtion
+	multicore_launch_core1(Core1Entry);
+	
 	// PIO Program initialize
 	pio = pio0;
 
@@ -135,40 +120,33 @@ int main() {
 
 	irq_set_exclusive_handler(PIO0_IRQ_0, WriteSidReg);
 	irq_set_enabled(PIO0_IRQ_0, true);
-	
+
 	pio0_hw->inte0 = PIO_IRQ0_INTE_SM0_BITS | PIO_IRQ0_INTE_SM1_BITS;
-
-	// Init Audio PWM
-	InitPWMAudio(AUDIO_PIN);
-	
-	// UART Start Message PicoSID
-	printf("The Pico SID by Thorsten Kattanek 1\n");
-
-	// Start Core#1 for SID Emualtion
-	multicore_launch_core1(Core1Entry);
 
     while (1)
     {
+#ifdef RINGBUFFER_ENABLE	
+		if(register_ring_buffer_rpos < register_ring_buffer_wpos)
+		{
+			uint16_t incomming = register_ring_buffer[register_ring_buffer_rpos & 0xfff];
+			SidWriteReg(incomming >> 2, (incomming >> 7) & 0xff);
+			register_ring_buffer_rpos++;
+		}else if(register_ring_buffer_rpos > register_ring_buffer_wpos)
+		{
+			/// buffer overflow
+			register_ring_buffer_rpos = register_ring_buffer_wpos;
+			printf("Buffer Overflow\n");
+		}
+#endif
     }
 }
 
 void pwm_irq_handle()
 {
 	pwm_clear_irq(slice_num);
-/*
-	float out_sample1 = OscGetOutput(&sid1.Osc[0]) / (float)0xfff;
-	float out_sample2 = OscGetOutput(&sid1.Osc[1]) / (float)0xfff;
-	float out_sample3 = OscGetOutput(&sid1.Osc[2]) / (float)0xfff;
 
-	out_sample1 *= EnvGetOutput(&sid1.Env[0]) / (float)0xff;
-	out_sample2 *= EnvGetOutput(&sid1.Env[1]) / (float)0xff;
-	out_sample3 *= EnvGetOutput(&sid1.Env[2]) / (float)0xff;
-		
-	uint16_t sample_out = ((out_sample1 + out_sample2 + out_sample3) / 3.0f) * 0x7ff * sid1.VolumeOut;
-	
-	pwm_set_gpio_level(AUDIO_PIN, sample_out);
-*/
-	 pwm_set_gpio_level(AUDIO_PIN, FilterGetOutput(&sid1.Filter));
+	// Sample Output
+	pwm_set_gpio_level(AUDIO_PIN, SidTestOut() / (float)0xffff * 0x7ff);
 }
 
 void InitPWMAudio(uint audio_out_gpio)
